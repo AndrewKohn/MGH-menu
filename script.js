041d52c961ff5e27b6ec61fd19c47722f61ab806
itsRunic <87670203+itsRunic@users.noreply.github.com>
2022-03-24 02:51:22 -0500

    added setting array to html elements

--- script.js
+++ script.js
@@ -1,54 +1,63 @@
 'use strict';
 var weeklyMenu = [
-  {
-    morning: [`Donut`, `Fresh Banana`],
-    lunch: [`Egg Salad`, `Pickle/Grapes`],
-    dinner: [`Breaded Pork Chops`, `Buttered Potatoes`, `Broccoli`],
-  },
-  {
-    morning: [`Banana Bread`, `Yogurt w/ Fresh Banana & Strawberries`],
-    lunch: [`Grilled Cheese`, `Tomato Soup`],
-    dinner: [
-      `Rice`,
-      `Chicken Curry w/ Potatoes & Carrots`,
-      `Slice of Marble Cake`,
-    ],
-  },
-  {
-    morning: [`Cinnamon Toast`, `Yogurt w/ Fresh Banana & Blueberries`],
-    lunch: [`Hotdog w/ Bun`, `Baked Beans`, `Pickle`],
-    dinner: [`Penne Pasta w/ Spaghetti Meat Sauce`, `Steamed Broccoli`],
-  },
-  {
-    morning: [`Frosted Mini Wheats w/ Milk`, `Fresh Blueberries`],
-    lunch: [
+  [
+    [`Donut`, `Fresh Banana`],
+    [`Egg Salad`, `Pickle/Grapes`],
+    [`Breaded Pork Chops`, `Buttered Potatoes`, `Broccoli`],
+  ],
+  [
+    [`Banana Bread`, `Yogurt w/ Fresh Banana & Strawberries`],
+    [`Grilled Cheese`, `Tomato Soup`],
+    [`Rice`, `Chicken Curry w/ Potatoes & Carrots`, `Slice of Marble Cake`],
+  ],
+  [
+    [`Cinnamon Toast`, `Yogurt w/ Fresh Banana & Blueberries`],
+    [`Hotdog w/ Bun`, `Baked Beans`, `Pickle`],
+    [`Penne Pasta w/ Spaghetti Meat Sauce`, `Steamed Broccoli`],
+  ],
+  [
+    [`Frosted Mini Wheats w/ Milk`, `Fresh Blueberries`],
+    [
       `Loaded Salad (Iceberg lettuce, Carrots, Ham, Boiled Eggs, Strawberries, & Salad Dressing)`,
     ],
-    dinner: [`Hamburger`, `Fries`, `Pickle`],
-  },
-  {
-    morning: [`Banana Bread`, `Fresh Fruits`],
-    lunch: [`Tuna Sandwich`, `Tomato Soup`],
-    dinner: [`Meatloaf`, `Mashed Potato`, `Broccoli`],
-  },
-  {
-    morning: [`Oatmeal`, `Fresh Fruits`],
-    lunch: [`Turkey Sandwich`, `Garden Salad`],
-    dinner: [`Sloppy Joe`, `French Fries`],
-  },
-  {
-    morning: [`Cinnamon Toast`, `Scrambled Eggs`],
-    lunch: [`Mac and Cheese w/ Diced Hotdog`],
-    dinner: [`Rice`, `Stir-fry Chicken`],
-  },
+    [`Hamburger`, `Fries`, `Pickle`],
+  ],
+  [
+    [`Banana Bread`, `Fresh Fruits`],
+    [`Tuna Sandwich`, `Tomato Soup`],
+    [`Meatloaf`, `Mashed Potato`, `Broccoli`],
+  ],
+  [
+    [`Oatmeal`, `Fresh Fruits`],
+    [`Turkey Sandwich`, `Garden Salad`],
+    [`Sloppy Joe`, `French Fries`],
+  ],
+  [
+    [`Cinnamon Toast`, `Scrambled Eggs`],
+    [`Mac and Cheese w/ Diced Hotdog`],
+    [`Rice`, `Stir-fry Chicken`],
+  ],
 ];
 
-// console.log(weeklyMenu);
-// console.log(JSON.stringify(menu, null, 4));
+// Set weeklyMenu[day][mealTime][mealItem] to menu grid
+const setMenuItems = menu => {
+  for (let i = 0; i < menu.length; i++) {
+    for (let j = 0; j < 3; j++) {
+      for (const menuItem of menu[i][j]) {
+        document.getElementById(`meal--${i}-${j}`).insertAdjacentHTML(
+          'beforeend',
+          `
+        <p class="meal--text">${menuItem}</p>
+        `
+        );
+      }
 
-for (const day of weeklyMenu) {
-  console.log(day);
-  console.log(day.morning);
-  console.log(day.lunch);
-  console.log(day.dinner);
-}
+      // console.log(j, i, menu[i][j]);
+    }
+  }
+};
+
+// console.log(weeklyMenu);
+// console.log(JSON.stringify(weeklyMenu, null, 4));
+
+setMenuItems(weeklyMenu);
