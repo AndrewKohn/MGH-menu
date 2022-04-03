6afc384a636ddcdf0016777c686f05fa8a3d9c73
itsRunic <87670203+itsRunic@users.noreply.github.com>
2022-04-03 12:56:26 -0500

    updated form submission events

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 'use strict';
 //  [TODO]
-//  ✅  Create a nav list when clicking on the menu button
-//  ✅  Nav menu should include all present menus
-//  ✅  Have an event handler for when the user clicks outside of the nav menu in order to close it
-//    Nav menu should include form to auto e-mail new submissions
+//   ✅ Create a nav list when clicking on the menu button
+//   ✅ Nav menu should include all present menus
+//   ✅ Have an event handler for when the user clicks outside of the nav menu in order to close it
+//   ✅ Nav menu should include form to auto e-mail new submissions
 //    Add hover effects over day/meal according to respective hovered item.
-
-// console.log(weeklyMenu);
-// console.log(JSON.stringify(weeklyMenu, null, 4));
 
 const overlay = document.querySelector(`.overlay`);
 const navBtn = document.querySelector(`.nav-menu-btn`);
@@ -21,9 +18,17 @@ const listMenu2 = document.querySelector(`.list--menu-1`);
 const listMenu3 = document.querySelector(`.list--menu-2`);
 const menuNumberEl = document.querySelector(`.menu-number`);
 const formSubmitBtn = document.querySelector(`.form-submit-btn`);
+const formContainerEl = document.querySelector(`.form-container`);
+const addMenuBtn = document.querySelector(`.add-menu-btn`);
+const closeFormBtn = document.querySelector(`.close-form-btn`);
+const lastUpdatedEl = document.querySelector(`.last-updated`);
 
 let count = 0;
+// var html = [];
 
+/////////////
+// METHODS //
+/////////////
 // Set menu[day][mealTime][mealItem] to menu grid
 const setMenuItems = menu => {
   for (let i = 0; i < menu.length; i++) {
@@ -79,34 +84,77 @@ const closeMenu = () => {
   }, 500);
 };
 
-//  [TODO]  if string contains a `,`, then split it to a new string
+const formOpen = () => {
+  formContainerEl.classList.toggle(`anim--fade-out`);
+  formContainerEl.classList.toggle(`anim--fade-in`);
+  formContainerEl.classList.toggle(`hidden`);
+};
+
+const formClose = () => {
+  formContainerEl.classList.toggle(`anim--fade-out`);
+  formContainerEl.classList.toggle(`anim--fade-in`);
+
+  // 0.5s delay to animation performance
+  setTimeout(() => {
+    formContainerEl.classList.toggle(`hidden`);
+  }, 500);
+};
+
+// Returns submission info and/or separates strings if more than one menu item in meal.
 const getSubmissionInfo = (currentDay, currentMeal) => {
   const submitDataInput = document.getElementById(
     `form-input--${currentDay}-${currentMeal}`
   );
 
-  if (submitDataInput.textContent.includes(`,`)) {
-    return submitDataInput.value;
+  if (submitDataInput.value.includes(`,`)) {
+    let trimmedMealArray = [];
+
+    const arr = submitDataInput.value.split(`,`);
+    for (const mealItem of arr) {
+      trimmedMealArray.push(mealItem.trim());
+    }
+    return trimmedMealArray;
   } else {
-    return `[` + submitDataInput.value + `]`;
+    let mealStrtoArr = [];
+    mealStrtoArr.push(submitDataInput.value.trim());
+    return mealStrtoArr;
   }
 };
 
-// INIT ON STARTUP
-setMenuItems(menu1);
+// Translates to html for menu form submission
+const createSubmissionHTML = () => {
+  let htmlSubmission = [];
+  for (let day = 0; day < 7; day++) {
+    let currentDayArr = [];
+    for (let mealTime = 0; mealTime < 3; mealTime++) {
+      // console.log(day, mealTime, getSubmissionInfo(day, mealTime));
+      currentDayArr.push(getSubmissionInfo(day, mealTime));
+    }
+    htmlSubmission.push(currentDayArr);
+  }
 
+  // console.log(htmlSubmission);
+  return JSON.stringify(htmlSubmission, null, 4);
+};
+
+// Change Last Updated html text
+const updateTimeDate = () => {
+  lastUpdatedEl.textContent = `Last updated: APR 03, 2022`;
+};
+
+/////////////////////
+// INIT ON STARTUP //
+/////////////////////
+setMenuItems(menu1);
+updateTimeDate();
+
+/////////////////////
 // EVENT LISTENERS //
+/////////////////////
 // Menu Button Events
 overlay.addEventListener(`click`, closeMenu);
 navBtn.addEventListener(`click`, openMenu);
 navBtnOpened.addEventListener(`click`, closeMenu);
-
-// Menu grid hover effect
-// menuGrid.addEventListener(`mouseover`, e => {
-//   if (menuItem === e.target) {
-//     menuItem.classList.add(`menu-item--hover`);
-//   }
-// });
 
 // Menu List Events
 listMenu1.addEventListener(`click`, function () {
@@ -132,11 +180,15 @@ listMenu3.addEventListener(`click`, function () {
 
 // Form Submission Event
 formSubmitBtn.addEventListener(`click`, e => {
-  e.preventDefault();
+  const html = createSubmissionHTML();
 
-  for (let day = 0; day < 7; day++) {
-    for (let mealTime = 0; mealTime < 3; mealTime++) {
-      console.log(day, mealTime, getSubmissionInfo(day, mealTime));
-    }
-  }
+  // sets to a hidden input that can be emailed when submit btn pressed
+  document.querySelector(`.html-input`).value = html;
 });
+
+addMenuBtn.addEventListener(`click`, function () {
+  formOpen();
+  closeMenu();
+});
+
+closeFormBtn.addEventListener(`click`, formClose);
